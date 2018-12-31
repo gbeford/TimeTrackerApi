c5c351e9dbe090d237695ad7d5a1458c284ff53a
gbeford <gbeford@gmail.com>
2018-12-31 16:19:38 -0500

    add jwt token

--- TimeTrackerAPI/Models/SecurityManagerService.cs
+++ TimeTrackerAPI/Models/SecurityManagerService.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 using TimeTrackerAPI.Models;
 
 
@@ -52,6 +56,34 @@ namespace TimeTrackerAPI.Security
             return list;
         }
 
+        protected string BuildJwtToken(AppUserAuth authUser)
+        {
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_settings.Key));
+
+            //Create standard JWT claims
+            List<Claim> jwtClaims = new List<Claim>();
+            jwtClaims.Add(new Claim(JwtRegisteredClaimNames.Sub, authUser.UserName));
+            jwtClaims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+
+            // add custom claim
+            jwtClaims.Add(new Claim("isAuthenticated", authUser.IsAuthenticated.ToString().ToLower()));
+            jwtClaims.Add(new Claim("CanAccess_Admin", authUser.CanAccess_Admin.ToString().ToLower()));
+            jwtClaims.Add(new Claim("CanAccess_Student", authUser.CanAccess_Student.ToString().ToLower()));
+
+            // Create the JwtSecurityToken object
+            var token = new JwtSecurityToken(
+                issuer: _settings.Issuer,
+                audience: _settings.Audience,
+                claims: jwtClaims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMinutes(_settings.MinutesToExpiration),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+            //Create a string representation of the Jwt token
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
         public AppUserAuth BuildUserAuthObject(AppUser authUser)
         {
             AppUserAuth ret = new AppUserAuth();
@@ -80,6 +112,8 @@ namespace TimeTrackerAPI.Security
 
                 }
             }
+            ret.BearerToken = BuildJwtToken(ret);
+
             return ret;
 
         }
