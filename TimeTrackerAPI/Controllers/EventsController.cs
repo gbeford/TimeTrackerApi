742013bfdd9d114e3abe3c02356c203e6fb6267f
gbeford <gbeford@gmail.com>
2019-01-14 15:57:16 -0500

    start padding out events CRUD

--- TimeTrackerAPI/Controllers/EventsController.cs
+++ TimeTrackerAPI/Controllers/EventsController.cs
@@ -29,6 +29,85 @@ namespace TimeTrackerAPI.Controllers
             return await events.OrderBy(e => e.SortOrder).ToListAsync();
         }
 
+        // PUT: api/Events/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvent(int id, Event event)
+        {
+
+            var oldEvent = await ctx.Events.FindAsync(id);
+            if (oldEvent == null)
+                return BadRequest();
+            if (oldEvent.EventID != id)
+                return BadRequest();
+
+            try
+            {
+                oldEvent.Description = event.Description;
+                ctx.Update(oldEvent);
+                await ctx.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Messages
+        [HttpPost]
+        public async Task<IActionResult> PostEvent(Event event)
+        {
+            try
+            {
+                // if (!ModelState.IsValid)
+                // {
+                //     return BadRequest(ModelState);
+                // }
+
+                ctx.Messages.Add(event);
+                await ctx.SaveChangesAsync();
+
+                return CreatedAtAction("GetMessage", new { id = event.EventID }, event);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // DELETE: api/Messages/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEvent(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var event = await ctx.Events.FindAsync(id);
+            if (event == null)
+            {
+                return NotFound();
+            }
+
+            ctx.Messages.Remove(event);
+            await ctx.SaveChangesAsync();
+
+            return Ok(event);
+        }
+private bool EventExists(int id)
+{
+    return ctx.Events.Any(e => e.EventID == id);
+}
+
     }
 }
 
